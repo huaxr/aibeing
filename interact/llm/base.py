483c0c7bf6d3b635e36b1459ff0a5ca203e6ad1d
huaxinrui <huaxinrui@tal.com>
2023-08-30 18:55:53 +0800

    first commit

--- interact/llm/base.py
+++ interact/llm/base.py
@@ -286,7 +286,7 @@ class AIBeingBaseTask(object):
     async def async_proxy(self, messages:List, hook:Union[Hook,None]=None, temperature:float=0.7, streaming:bool=False, functions: List=None, model_name:str = "msai") -> Any:
         assert len(messages) > 0, "messages length must > 0"
         if functions:
-            temperature = 0.03
+            temperature = 0.1
 
         headers, data, streaming, llm_addr = self.prepare_proxy_header_data(messages, streaming, temperature, functions, model_name=model_name)
         async with aiohttp.ClientSession() as session:
