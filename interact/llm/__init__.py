9d9ec6e7a4f32f678c696f674abbdcf0c19eda21
huaxinrui <huaxinrui@tal.com>
2023-08-30 10:57:34 +0800

    xxx

--- interact/llm/__init__.py
+++ interact/llm/__init__.py
@@ -5,12 +5,12 @@ from interact.schema.chat import response
 from interact.schema.protocal import protocol
 
 def check_running(func):
-    def wrapper(self, inputs, **kwargs):
+    def wrapper(self, *args, **kwargs):
         func_flag = func.__name__ + "_flag"
         if not hasattr(self, func_flag):
             setattr(self, func_flag, None)
             try:
-                res = func(self, inputs, **kwargs)
+                res = func(self, *args, **kwargs)
             finally:
                 delattr(self, func_flag)
             return res
